2cebf3561b52f0fa992f13a4d041d4aea169ef18
dejo-hashicorp <171065576+dejo-hashicorp@users.noreply.github.com>
2024-11-13 12:45:32 -0700

    Update deploy_app.sh

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -13,7 +13,7 @@ cat << EOM > /var/www/html/index.html
     <img src="${PLACEHOLDER}" style="max-width: 600px; max-height: 400px;">
   </center>
   <center><h2>Meow World!</h2></center>
-  Welcome to ${PREFIX}'s app. Replace this text with your own. Hi JDCP! 
+  Welcome to ${PREFIX}'s app. Replace this text with your own. Hi ATB Gerry Joe and Moh!! 
   <!-- END -->
 
   </div>
